098e02e31304513bfc93f7ca415467168df254d7
nico_masilla <nicomasilla@gmail.com>
2021-10-18 17:25:13 +0200

    init pokedex project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	let data = [];
+	const [state, setState] = useState({
+		pokemonArray: data,
+		start: 0,
+		end: 20,
+	});
+
+	useEffect(() => {
+		axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`)
+			.then(response => {
+				data = response.data.results;
+				setState({
+					...state,
+					pokemonArray: data
+				})
+			})
+		console.log('component did mount');
+	}, [])
+
+	const nextPage = () => {
+		if (state.start >= 151) {
+			return;
+		}
+		setState({
+			...state,
+			start: state.start += 20,
+			end: state.end += 20,
+		})
+	}
+	const prevPage = async () => {
+		if (state.start <= 0) {
+			return;
+		}
+		setState({
+			...state,
+			start: state.start -= 20,
+			end: state.end -= 20,
+		})
+	}
+
+	const viewPokemonDetail = (pokemonName) => () => {
+		axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+			.then(response => {
+				data = response.data;
+				console.log(data);
+			})
+	}
+
+	return (
+		<div className="App">
+			{
+				state.pokemonArray.slice(state.start, state.end).map((item, index) => {
+					return (
+						<div style={{cursor: 'pointer'}} key={index} onClick={viewPokemonDetail(item.name)}>{item.name}</div>
+					)
+				})
+			}
+			<button onClick={nextPage}>next page</button>
+			<button onClick={prevPage}>previous page</button>
+
+		</div>
+	);
 }
 
 export default App;
