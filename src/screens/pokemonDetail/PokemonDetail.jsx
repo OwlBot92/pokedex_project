dda1cb71bea7d3e40d0facb45492004be3444ba3
nico_masilla <nicomasilla@gmail.com>
2021-10-19 13:15:56 +0200

    UPDATE: not calling the api anymore, gets data from the parent

--- src/screens/pokemonDetail/PokemonDetail.jsx
+++ src/screens/pokemonDetail/PokemonDetail.jsx
@@ -4,35 +4,20 @@ import React, { useEffect, useState } from 'react';
 
 const PokemonDetail = (props) => {
     /* console.log(props.location.state.id); */
-
-    const [pokemonInfo, setPokemonInfo] = useState({});
+    let routePokeData = props.location.state.id
+    const [pokemonInfo, setPokemonInfo] = useState(routePokeData);
     const [dataReceived, setDataReceived] = useState(false);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-
-    let pokemonName = props.location.state.id
-
-    const fetchData = async () => {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        setPokemonInfo(response.data)
-        setDataReceived(true)
-    }
-
-
+    
     return (
         <>
-            {dataReceived &&
+           
                 <div>
-                    {console.log(pokemonInfo)}
-                    <h1>{pokemonName}</h1>
-                    <span>Weight: {pokemonInfo.weight}</span> <br />
-                    <span>Height: {pokemonInfo.height}</span>
-                    <img src={pokemonInfo.sprites.other['official-artwork'].front_default} alt="" />
+                    <h1>{pokemonInfo.name}</h1>
+                    <div>Weight: {pokemonInfo.size.weight}</div> <br />
+                    <div>Height: {pokemonInfo.size.height}</div>
+                    <img src={pokemonInfo.img} alt="" />
                 </div>
-            }
         </>
     );
 }
